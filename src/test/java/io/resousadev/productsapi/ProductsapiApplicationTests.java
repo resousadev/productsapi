630f5f33a1e418ffff7cbf1217f4a20b3dc59b54
resousadev <resousadev@gmail.com>
2026-03-23 23:36:43 -0300

    Initialize Spring Boot project with Gradle setup and basic application structure

---
+++ src/test/java/io/resousadev/productsapi/ProductsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.resousadev.productsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
