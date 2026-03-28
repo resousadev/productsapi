c5ec71da1c709575650f97ab27f7da11a94139d4
renanrcs <renan.cavalcante@yahoo.com>
2026-03-28 18:41:47 -0300

    Enhance Product model with JPA annotations for database mapping

--- src/main/java/io/resousadev/productsapi/model/Product.java
+++ src/main/java/io/resousadev/productsapi/model/Product.java
@@ -1,15 +1,25 @@
 package io.resousadev.productsapi.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-@Getter
-@Setter
+@Entity
+@Table(name = "products")
 public class Product {
 
+    @Id
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "price")
     private Double price;
 
 }
